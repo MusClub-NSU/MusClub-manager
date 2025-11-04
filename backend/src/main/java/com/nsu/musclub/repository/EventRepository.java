55e978e55e9f8dc4f154e37221a3ec45a570ef3c
animovscw <k.s.a.basketball@gmail.com>
2025-11-04 14:39:15 +0700

    Added repositories

--- backend/src/main/java/com/nsu/musclub/repository/EventRepository.java
+++ backend/src/main/java/com/nsu/musclub/repository/EventRepository.java
@@ -1,7 +1,9 @@
 package com.nsu.musclub.repository;
 
 import com.nsu.musclub.domain.Event;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
+    List<Event> findByParentId(Long parentId);
 }
