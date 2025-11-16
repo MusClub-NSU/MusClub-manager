5495bdb39f295927936ce62e2cf32739237ab7a5
Cookie <2389397239@qq.com>
2025-11-16 20:57:19 +0700

    the report in build/reports/tests/test/index.html

--- backend/src/test/java/com/nsu/musclub/AbstractIntegrationTest.java
+++ backend/src/test/java/com/nsu/musclub/AbstractIntegrationTest.java
@@ -16,12 +16,13 @@ public abstract class AbstractIntegrationTest {
     static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16")
             .withDatabaseName("musclub_test")
             .withUsername("musclub")
-            .withPassword("musclub");
+            .withPassword("musclub")
+            .withReuse(false);
 
     @DynamicPropertySource
     static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgres::getJdbcUrl);
-        registry.add("spring.datasource.username", postgres::getUsername);
-        registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.datasource.url", () -> postgres.getJdbcUrl());
+        registry.add("spring.datasource.username", () -> postgres.getUsername());
+        registry.add("spring.datasource.password", () -> postgres.getPassword());
     }
 }
